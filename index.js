68b8360fb4d6cde2603ee45abf3adaf6594eb11d
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-20 16:01:47 +0100

    url routing + dummy data

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 // Resuable server set up
 
 const http = require('http');
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    res.end (`yo Dan - you're on the server`)
+    const pathName = (req.url)
+   
+    if(pathName === '/' || pathName === '/overview') {
+        res.end("Your team overview");
+    } else if (pathName === '/team') {
+        res.end("Specific team overview")
+    } else if (pathName === './api') {
+        res.end('API');
+    }
+     else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-own-header': 'STOP-GAMER'
+        });
+        res.end(<h2>PAGE NOT FOUND</h2>)
+    }
 }); 
 
 
@@ -11,4 +27,4 @@ server.listen(3000, '127.0.0.1', () => {
     console.log('listening to request on port 3000')
 }); // explicitly we can define the ip address for local host but will do it default also
 
-
+    
